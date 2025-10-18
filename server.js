a889fb3742127dffe8d3e66ea9b24254d9233ba3
trongkhang156 <132425547+trongkhang156@users.noreply.github.com>
2025-10-18 11:30:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,5 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
