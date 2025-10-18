ba7890364ca937a0e944f9a91833cf5d53de0c19
trongkhang156 <132425547+trongkhang156@users.noreply.github.com>
2025-10-18 08:11:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 4000;
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server chạy trên http://192.168.1.107:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
