514895d363b6a30925216c656016bd16887c931c
trongkhang156 <132425547+trongkhang156@users.noreply.github.com>
2025-10-21 14:26:54 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,8 @@
-// sw.js
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   event.waitUntil(
     clients.matchAll({ type: "window" }).then((clientList) => {
-      if (clientList.length > 0) {
-        // Nếu tab đã mở thì focus lại
-        return clientList[0].focus();
-      }
-      // Nếu chưa mở thì mở mới
+      if (clientList.length > 0) return clientList[0].focus();
       return clients.openWindow("/");
     })
   );
